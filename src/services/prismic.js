64c766bcf0f0a2a1cd1a6a31c8fb7019c20a4a47
Rafael Dias <rsdias84@gmail.com>
2024-09-30 08:55:16 -0300

    pagina em construção

--- src/services/prismic.js
+++
@@ -1,9 +0,0 @@
-import Prismic from '@prismicio/client';
-
-export function getPrismicClient(req) {
-  const prismic = Prismic.client(process.env.REACT_APP_PRISMIC_API_ENDPONT, {
-    req,
-    accessToken: process.env.REACT_APP_PRISMIC_ACCESS_TOKEN,
-  });
-  return prismic;
-}
